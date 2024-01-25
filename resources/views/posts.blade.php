ef76f79ce01b706c0c931e5784ef9f56ef60e6bc
Eduarda <eduardabpetry@gmail.com>
2024-01-25 22:47:19 +0100

    Extend layout and style articles section

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layout')
+
+@section('title')
     <title>Posts</title>
-</head>
-<body>
-    <h2>How PHP Divide Our Class in 2 Extremist Groups</h2>
-    <p>As the weeks pass by, the heat from PHP do not subside. Class is divides between hatred and mere tolerance, but there is no escape from PHP. He is here to stay - and there is nothing that we can do about that. Apart complaining. We can complain. A lot.</p>
-    <br>
-    <h2>Is Laravel the new Marvel?</h2>
-    <p>Laravel is the new kid in the block. And it sounds like "Marvel". But apart from the fonetics, they don't share any similarities.</p>
-    <br>
-    <h2>How Coffee Saved the World (again!)</h2>
-    <p>Another week, another Monday, and the developers apprentice's would have not survived without the help of the magic black liquid that powers their brains.</p>
-</body> 
-</html>
+@endsection
+
+@section('main')
+
+    <h3 class="ms-5">Articles</h3>
+    <div class="container text-center mt-4">
+        <div class="row gx-5">
+            <div class="col border border-danger p-2 m-2">
+                <h4>How PHP Divide Our Class in 2 Extremist Groups</h4>
+                <p>As the weeks pass by, the heat from PHP do not subside. Class is divides between hatred and mere tolerance, but there is no escape from PHP. He is here to stay - and there is nothing that we can do about that. Apart complaining. We can complain. A lot.</p>
+            </div>
+            <div class="col border border-danger p-2 m-2">
+                <h4>Is Laravel the new Marvel?</h4>
+                <p>Laravel is the new kid in the block. And it sounds like "Marvel". But apart from the fonetics, they don't share any similarities.</p>
+            </div>
+            <div class="col border border-danger p-2 m-2">
+                <h4>How Coffee Saved the World (again!)</h4>
+                <p>Another week, another Monday, and the developers apprentice's would have not survived without the help of the magic black liquid that powers their brains.</p>
+            </div>
+        </div>
+    </div>
+@endsection
